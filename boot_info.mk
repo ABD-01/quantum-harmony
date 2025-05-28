81ea72fc110c415650384c1e9b1addf239e1d0f9
ABD-01 <myac931@gmail.com>
2025-05-28 15:36:35 +0530

    update boot_info.mk for version+CRC32 bin generation

--- boot_info.mk
+++ boot_info.mk
@@ -4,13 +4,17 @@
 # File:		boot_info.mk
 # Author:	Muhammed Abdullah Shaikh <muhammed.shaikh@accoladeelectronics.com>
 # Date:		15 April 2025
-# Version:	1.1
+# Version:	1.2
 # Brief:	Builds a boot information hex file. Depends on application
 # 			bin file to generate boot info such as file size, crc etc.
 #
 # Copyright (c) 2024-2025 Accolade Electronics Pvt. Ltd. All Rights Reserved.
 #
 # Changelog:
+# 2025-05-27   Muhammed Abdullah Shaikh <muhammed.shaikh@accoladeelectronics.com>
+#   - (v1.2) Updated binary file to be generated from $(POST_BUILD_UTIL) rather
+#     rather than srec.
+#   - App version is now required for the bin file and is read from version.h file.
 #
 # 2025-04-23   Muhammed Abdullah Shaikh <muhammed.shaikh@accoladeelectronics.com>
 #   - (v1.1) Added targets "ti_firmware_with_appcrc.hex", corresponding bin and 
@@ -24,28 +28,30 @@
 #
 ##############################################################################
 
-SHELL 	= cmd.exe
+SHELL 		= cmd.exe
 
 EXT		= .exe
 RM 		= DEL /F
-RMDIR 	= RMDIR /S/Q
+RMDIR 		= RMDIR /S/Q
 
 TOOLCHAIN_PATH	:= C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-armllvm_4.0.1.LTS
-CC 				:= $(TOOLCHAIN_PATH)/bin/tiarmclang
-#LD 			:= $(TOOLCHAIN_PATH)/bin/tiarmlnk
-LD 				:= $(CC)
-OBJCOPY 		:= $(TOOLCHAIN_PATH)/bin/tiarmhex
+CC 		:= $(TOOLCHAIN_PATH)/bin/tiarmclang
+#LD 		:= $(TOOLCHAIN_PATH)/bin/tiarmlnk
+LD 		:= $(CC)
+OBJCOPY 	:= $(TOOLCHAIN_PATH)/bin/tiarmhex
 
-OUTPUT_DIR		:= build
-BOOTINFO_DIR 	:= ../boot_info
+OUTPUT_DIR	:= build
+BOOTINFO_DIR 	:= ../post_build_pipeline
 
 POST_BUILD_UTIL	:= $(BOOTINFO_DIR)/generate_bootinfo_file$(EXT)
-SREC_CAT 		:= srec_cat$(EXT)
-SREC_CMP 		:= srec_cmp$(EXT)
+SREC_CAT 	:= srec_cat$(EXT)
+SREC_CMP 	:= srec_cmp$(EXT)
 
-APP_HEX			:= ti_firmware.hex
-APP_BIN			:= ti_firmware.bin
+APP_HEX		:= ti_firmware.hex
+APP_BIN		:= ti_firmware.bin
 LINKER_SCRIPT 	:= $(BOOTINFO_DIR)/linker.cmd
+VERSION_H	:= ..\\src\\application\\version.h
+APP_VERSION	:= $(shell for /f "tokens=3 delims= " %%A in ('findstr /C:APP_VERSION_STRING $(VERSION_H) 2^>NUL') do @echo %%~A)
 
 BOOTINFO_SRC 	:= boot_info_data.c
 BOOTINFO_OBJ 	:= $(OUTPUT_DIR)/boot_info_data.o
@@ -53,11 +59,10 @@ BOOTINFO_ELF 	:= $(OUTPUT_DIR)/boot_info.out
 BOOTINFO_HEX 	:= $(OUTPUT_DIR)/boot_info.hex
 BOOTINFO_BIN 	:= $(OUTPUT_DIR)/boot_info.bin
 
-CMD_FILE1	 	 := srec_cmd_create_hex_with_appcrc.txt
-CMD_FILE2 		 := srec_cmd_create_bin_with_appcrc.txt
-APP_CRC_BIN		 := app_crc32.bin
-APP_HEX_WITH_CRC := ti_firmware_with_appcrc.hex
-APP_BIN_WITH_CRC := ti_firmware_with_appcrc.bin
+CMD_FILE	:= srec_cmd_create_hex.txt
+APP_CRC_BIN	:= app_crc32.bin
+APP_BIN_NEW	:= ti_firmware_ver_and_appcrc.bin
+APP_HEX_NEW	:= ti_firmware_ver_and_appcrc.hex
 
 DEVICE		:= MSPM0G3507
 CPU_FLAGS	:= -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb
@@ -77,9 +82,9 @@ $(error "$(SREC_CAT) not found. Install srec_cat or add to PATH if installed alr
 endif
 
 
-.PHONY: all clean
+.PHONY: all run_util clean
 
-all: $(BOOTINFO_HEX) $(BOOTINFO_BIN) $(APP_HEX_WITH_CRC) $(APP_BIN_WITH_CRC)
+all: $(BOOTINFO_HEX) $(BOOTINFO_BIN) $(APP_HEX_NEW) $(APP_BIN_NEW)
 
 $(OUTPUT_DIR):
 	@if not exist "$(OUTPUT_DIR)" mkdir "$(OUTPUT_DIR)"
@@ -108,27 +113,19 @@ $(BOOTINFO_OBJ): $(BOOTINFO_SRC) | $(OUTPUT_DIR)
 	@echo 'Finished compiling source: "$@"'
 	@echo ' '
 
-$(BOOTINFO_SRC): $(APP_BIN) $(POST_BUILD_UTIL)
-	@echo 'Creating source file: "$@"'
-	$(POST_BUILD_UTIL) $<
-	@echo 'Finished creating file: "$@"'
-	@echo ' '
-
-$(APP_BIN_WITH_CRC): $(APP_HEX) $(APP_CRC_BIN) $(CMD_FILE2)
-	@echo 'Creating BIN file with CRC: "$@"'
-	$(SREC_CAT) $< @$(CMD_FILE2) $@ -Binary
-	@echo 'Finished creating file: "$@"'
-	@echo 'Comparing files: "$(APP_BIN)" and "$@"'
-	@$(SREC_CMP) $(APP_BIN) -Binary $@ -Binary -Verbose || true
+$(BOOTINFO_SRC) $(APP_BIN_NEW) $(CMD_FILE): | run_util
+run_util: $(POST_BUILD_UTIL)  $(APP_BIN)
+	@echo 'Running "$<"'
+	@$< $(APP_BIN) $(APP_VERSION)
 	@echo ' '
 
 
-$(APP_HEX_WITH_CRC): $(APP_HEX) $(APP_CRC_BIN) $(CMD_FILE1)
-	@echo 'Creating HEX file with CRC: "$@"'
-	$(SREC_CAT) $< @$(CMD_FILE1) $@ -Intel
+$(APP_HEX_NEW): $(CMD_FILE) $(APP_BIN_NEW)
+	@echo 'Creating HEX file: "$@"'
+	$(SREC_CAT) @$<
 	@echo 'Finished creating file: "$@"'
 	@echo ' '
 
 clean:
 	-@$(RMDIR) $(OUTPUT_DIR)
-	-@$(RM) $(BOOTINFO_SRC) $(APP_CRC_BIN) $(APP_HEX_WITH_CRC) $(APP_BIN_WITH_CRC) $(CMD_FILE1) $(CMD_FILE2)
+	-@$(RM) $(BOOTINFO_SRC) $(APP_CRC_BIN) $(APP_HEX_NEW) $(APP_BIN_NEW) $(CMD_FILE)
